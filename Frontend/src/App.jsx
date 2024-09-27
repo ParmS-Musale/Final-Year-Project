7d25f56292f7ef4fa030598fc9361e1580f0b302
vaishu1109 <vaishu.sg1109@gmail.com>
2024-09-27 19:28:28 +0530

    Nvbar and footer

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
+
 
 const App = () => {
   return (
-    <div className='text-3xl font-bold underline'>
-      vaishnavi
-    </div>
+    <>
+    <Navbar/>
+    <Footer/>
+    </>
   )
 }
 
